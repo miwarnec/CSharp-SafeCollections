36b34b59ec33d50c661db10006f6b76c28d8c8f5
mischa <16416509+vis2k@users.noreply.github.com>
2024-04-20 19:37:07 +0800

    unity log exc

--- SafeCollections/SafeCollections/SafeHashSet.cs
+++ SafeCollections/SafeCollections/SafeHashSet.cs
@@ -39,8 +39,14 @@ namespace SafeCollections
     {
       if (enumerating)
       {
-        throw new InvalidOperationException(
+        InvalidOperationException exception = new InvalidOperationException(
           "Attempted to access collection while it's being enumerated elsewhere. This would cause an InvalidOperationException when enumerating, which would cause a race condition which is hard to debug.");
+#if UNITY_2019_1_OR_NEWER
+                // in Unity: log but continue so the game behaves as before but adds the obvious exception message
+                UnityEngine.Debug.LogException(exception);
+#else
+        throw exception;
+#endif
       }
     }
     // END CUSTOM CHANGE
