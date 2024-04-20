de5be201470540b142a9cecefc2fdbfc9cc4707a
mischa <info@noobtuts.com>
2024-04-20 15:16:10 +0800

    empty project

---
+++ SafeCollections/SafeCollections.Tests/Tests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace SafeCollections.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
