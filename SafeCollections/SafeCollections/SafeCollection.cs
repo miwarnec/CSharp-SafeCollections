5b39a40b1d013638e2d3ea120260aa987b1e35ea
mischa <info@noobtuts.com>
2024-04-22 23:10:37 +0800

    add thread id check

--- SafeCollections/SafeCollections/SafeCollection.cs
+++ SafeCollections/SafeCollections/SafeCollection.cs
@@ -3,12 +3,16 @@ namespace SafeCollections
 {
     public abstract class SafeCollection
     {
+        // flag & thread id from the thread that is currently enumerating.
+        // threadId is useful to debug race conditions with multiple threads.
         bool enumerating = false;
+        int enumeratingThreadId = 0;
 
         // call this when beginning / ending enumeration
         protected void BeginEnumerating()
         {
           enumerating = true;
+          enumeratingThreadId = Thread.CurrentThread.ManagedThreadId;
         }
 
         protected void EndEnumerating()
@@ -21,8 +25,9 @@ namespace SafeCollections
         {
           if (enumerating)
           {
+            int threadId = Thread.CurrentThread.ManagedThreadId;
             InvalidOperationException exception = new InvalidOperationException(
-              "Attempted to access collection while it's being enumerated elsewhere. This would cause an InvalidOperationException when enumerating, which would cause a race condition which is hard to debug.");
+              $"Attempted to access collection from ThreadId={threadId} while it's being enumerated elsewhere from ThreadId={enumeratingThreadId}. This would cause an InvalidOperationException when enumerating, which would cause a race condition which is hard to debug.");
     #if UNITY_2019_1_OR_NEWER
                     // in Unity: log but continue so the game behaves as before but adds the obvious exception message
                     UnityEngine.Debug.LogException(exception);
